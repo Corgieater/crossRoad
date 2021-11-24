47a4183561f89c48656c628190e3147658e453dc
Fan, Chia-Hua <92343813+Corgieater@users.noreply.github.com>
2021-11-24 21:18:11 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.up, "Up")
+screen.onkey(player.down, "Down")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.make_car()
+    car_manager.move_cars()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    if player.is_at_finish_line():
+        scoreboard.increase_level()
+        player.go_to_start()
+        car_manager.speed_up()
+
+
+screen.exitonclick()
